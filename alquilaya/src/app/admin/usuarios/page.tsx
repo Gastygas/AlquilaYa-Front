23967cad8bf3d18f9845c105a9af869934a5f568
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-10-22 09:11:28 -0500

    dashboard de usuarios listo

--- alquilaya/src/app/admin/usuarios/page.tsx
+++ alquilaya/src/app/admin/usuarios/page.tsx
@@ -1,33 +1,62 @@
 import HeaderAdmin from '@/Components/HeaderAdmin/HeaderAdmin'
-import { IUser } from '@/Interfaces/IUser';
 
-const page = async() => {
+const page = async () => {
+  const url = "http://localhost:3001/users";
 
-const url = "http://localhost:3001/users"
+  const res = await fetch(url, {
+    method: "GET",
+    cache: "no-store",
+  });
 
- const getUsers = await fetch(url,{
-      method: "GET",
-  }).then(users => users.json())
-    .catch(err => console.log(err))
+  if (!res.ok) {
+    throw new Error('Error al obtener los usuarios');
+  }
+
+  const users = await res.json();
+
+  /*const url = "http://localhost:3001/users"
   
-  
+   const getUsers = await fetch(url,{
+        method: "GET",
+    }).then(users => users.json())
+      .catch(err => console.log(err))
+    */
+
   return (
     <div>
-    <HeaderAdmin />
-    <div className='container'>
+      <HeaderAdmin />
+      <div className='container'>
         <div className='padding-section'>
-        <h1 className="pb-12 text-primary">Usuarios</h1>
-        {getUsers.map((user:any) => {
-          return(
-            <div>
-              <h2>{user.name}</h2>
-              <h2>{user.surname}</h2>
-            </div>
-          )
-        })}
-        </div>
+          <h1 className="pb-12 text-primary">Usuarios</h1>
+          <table className="min-w-full bg-white border border-gray-200">
+            <thead>
+              <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                <th className="py-3 px-6 text-left">Nombre</th>
+                <th className="py-3 px-6 text-left">Email</th>
+                <th className="py-3 px-6 text-center">Dni</th>
+                <th className="py-3 px-6 text-center">Country</th>
+                <th className="py-3 px-6 text-center">Dirección</th>
+                <th className="py-3 px-6 text-center">Teléfomo</th>
+
+              </tr>
+            </thead>
+            <tbody className="text-gray-600 text-sm font-light"></tbody>
+            {users.map((user: any, i: any) => {
+              return (
+                <tr key={i} className="border-b border-gray-200 hover:bg-gray-100">
+                  <td className="py-3 px-6 text-left text-primary">{user.name} {user.surname}</td>
+                  <td className="py-3 px-6 text-left">{user.email}</td>
+                  <td className="py-3 px-6 text-center">{user.dni}</td>
+                  <td className="py-3 px-6 text-center">{user.country}</td>
+                  <td className="py-3 px-6 text-center">{user.address}</td>
+                  <td className="py-3 px-6 text-center">{user.role}</td>
+                </tr>
+              )
+            })}
+        </table>
+      </div>
     </div>
-</div>
+</div >
   )
 }
 
