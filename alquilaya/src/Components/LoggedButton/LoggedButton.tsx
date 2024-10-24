48e4855e00f62579422b55dd0075ccb728523ce5
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-10-23 22:04:24 -0500

    error de validación en register corregido

--- alquilaya/src/Components/LoggedButton/LoggedButton.tsx
+++ alquilaya/src/Components/LoggedButton/LoggedButton.tsx
@@ -4,7 +4,7 @@ import Button from "../Button/Button"
 import NotLoggedButtons from "../NotLoggedButtons/NotLoggedButtons";
 import { useContext } from "react";
 import AuthContext from "../contexts/authContext";
-import { IoLogOut } from "react-icons/io5";
+import { MdLogout } from "react-icons/md";
 
 const LoggedButton = () => {
  const { user,logout } = useContext(AuthContext);
@@ -12,7 +12,7 @@ const LoggedButton = () => {
 return (
   <div className="flex flex-row justify-around items-center">
           <Link href="/"><Button variant="primary" className="font-bold text-secondary">Mi cuenta</Button></Link>
-      <Button variant="transparent" onClick={logout} className="underline text-primary">Salir<IoLogOut size={20} color="--var(--darkBlue)"/></Button>
+      <Button variant="transparent" onClick={logout} className="text-primary font-semibold">Salir<MdLogout size={20} color="--var(--darkBlue)"/></Button>
   </div>
 )};return (<NotLoggedButtons/>)
 }
