fe47c7c64bb764a8bceaa0f83e3803dd9755c5b5
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-10-24 18:35:29 -0500

    estilos sube tu propiedad iniciados

--- alquilaya/src/Components/Sube-tu-propiedad/Step-1.tsx
+++ alquilaya/src/Components/Sube-tu-propiedad/Step-1.tsx
@@ -5,11 +5,11 @@ import ButtonCyan from '../ButtonCyan/ButtonCyan'
 const Step1 = () => {
     return (
 
-        <div className="relative bg-gray-100 min-h-screen p-10">
-            <h1 className="mt-20 text-black  mb-4">Paso 1: Describir la propiedad</h1>
+        <div className="relative min-h-screen">
+            <h1 className=" text-black  mb-4">Paso 1: Describir la propiedad</h1>
             <textarea
                 placeholder="Máximo 500 caracteres"
-                className="mt-20 ml-60 w-full max-w-4xl h-44 p-4 bg-gray-50 border-2 border-[#aa31cf] focus:border-[#2CFFDE] hover:border-[#2CFFDE] focus:outline-none transition duration-200 rounded-lg resize-none"
+                className="mt-20 w-full max-w-4xl h-44 p-4 bg-gray-50 border-2 border-[#aa31cf] focus:border-[#2CFFDE] hover:border-[#2CFFDE] focus:outline-none transition duration-200 rounded-lg resize-none"
                 maxLength={500}
             />
 
