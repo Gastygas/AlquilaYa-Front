b3f212902515c2e4e27545386e494593f0a7cd69
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-10-24 23:39:30 -0500

    dashboard usuario mockeado

--- alquilaya/src/Components/UserInformationCard/UserInformationCard.tsx
+++ alquilaya/src/Components/UserInformationCard/UserInformationCard.tsx
@@ -1,23 +1,30 @@
+import styles from "./UserInformationCard.module.css"
 
 const UserInformationCard = () => {
   return (
-         <div className='flex flex-row'>
-                  <div className='w-1/2 flex flex-col opacity-80'>
-                    <h4 className='pt-4 text-center font-bold'>Nombre</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                    <h4 className='pt-4 text-center font-bold'>Email</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                    <h4 className='pt-4 text-center font-bold'>Dni</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                  </div>
-                  <div className='w-1/2 flex flex-col opacity-80'>
-                    <h4 className='pt-4 text-center font-bold'>País</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                    <h4 className='pt-4 text-center font-bold'>Dirección</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                    <h4 className='pt-4 text-center font-bold'>Teléfono</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                  </div>
+    <div className='w-2/3 flex flex-col bg-cyan-100  p-4 rounded-2xl justify-center align-middle'>
+    <h3 className='text-primary text-center opacity-1 pb-4'>Tu Información</h3>
+    <div className='flex flex-row'>
+      <div className='w-1/2 flex flex-col opacity-80'>
+        <h4 className='pt-4 text-center font-bold'>Nombre</h4>
+        <h4 className='pb-4 text-center'>Ejemplo1</h4>
+        <h4 className='pt-4 text-center font-bold'>Email</h4>
+        <h4 className='pb-4 text-center'>Ejemplo1</h4>
+        <h4 className='pt-4 text-center font-bold'>Dni</h4>
+        <h4 className='pb-4 text-center'>Ejemplo1</h4>
+      </div>
+      <div className='w-1/2 flex flex-col opacity-80'>
+        <h4 className='pt-4 text-center font-bold'>País</h4>
+        <h4 className='pb-4 text-center'>Ejemplo1</h4>
+        <h4 className='pt-4 text-center font-bold'>Dirección</h4>
+        <h4 className='pb-4 text-center'>Ejemplo1</h4>
+        <h4 className='pt-4 text-center font-bold'>Teléfono</h4>
+        <h4 className='pb-4 text-center'>Ejemplo1</h4>
+      </div>
+
+    </div>
+    <button className={styles.editButton}>Editar</button>
+  </div>
   )
 }
 
--- alquilaya/src/app/mi-cuenta/page.tsx
+++ alquilaya/src/app/mi-cuenta/page.tsx
@@ -1,6 +1,10 @@
 import Header from '@/Components/Header/Header'
 import ProtectedRoute from '@/Components/ProtectRoutes/ProtecRoutes'
-import styles from "./mi-cuenta.module.css"
+import UserHistory from '@/Components/UserHistory/UserHistory'
+import UserInformationCard from '@/Components/UserInformationCard/UserInformationCard'
+import UserPropertiesFav from '@/Components/UserPropertiesFav/UserPropertiesFav'
+import UserPropertiesHistory from '@/Components/UserPropertiesHistory/UserPropertiesHistory'
+import Image from 'next/image'
 
 const page = () => {
   return (
@@ -8,36 +12,14 @@ const page = () => {
       <div>
         <Header />
         <div className='container'>
-          <div className='padding-section'>
-            <h1 className='pb-20'>Tu cuenta</h1>
+          <div className='padding-section gap-8'>
+            <h1 className='pb-12'>Tu cuenta</h1>
             <div className='flex flex-row w-full gap-8'>
-              <div className='w-2/3 flex flex-col bg-cyan-100  p-4 rounded-2xl justify-center align-middle'>
-                <h3 className='text-primary text-center opacity-1 pb-4'>Tu Información</h3>
-                <div className='flex flex-row'>
-                  <div className='w-1/2 flex flex-col opacity-80'>
-                    <h4 className='pt-4 text-center font-bold'>Nombre</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                    <h4 className='pt-4 text-center font-bold'>Email</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                    <h4 className='pt-4 text-center font-bold'>Dni</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                  </div>
-                  <div className='w-1/2 flex flex-col opacity-80'>
-                    <h4 className='pt-4 text-center font-bold'>País</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                    <h4 className='pt-4 text-center font-bold'>Dirección</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                    <h4 className='pt-4 text-center font-bold'>Teléfono</h4>
-                    <h4 className='pb-4 text-center'>Ejemplo1</h4>
-                  </div>
-
-                </div>
-                <button className={styles.editButton}>Editar</button>
-              </div>
-              <div className='w-1/3 bg-cyan-100 p-4 rounded-2xl'>
-              <h3>Favoritos</h3>
-              </div>
+              <UserInformationCard />
+              <UserPropertiesFav />
             </div>
+              <UserHistory/>
+              <UserPropertiesHistory/>
           </div>
         </div>
       </div>
