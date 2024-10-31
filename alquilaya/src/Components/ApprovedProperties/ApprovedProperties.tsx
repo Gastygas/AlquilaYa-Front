c6d804d3d07ac0ea724b8350fe01b522299e5aae
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-10-31 18:32:33 -0500

    función add button agregada

---
+++ alquilaya/src/Components/ApprovedProperties/ApprovedProperties.tsx
@@ -0,0 +1,60 @@
+"use client"
+import IProperty from "@/Interfaces/IProperties"
+import styles from "./Approve.module.css"
+
+interface PropertyTableProps {
+    properties: IProperty[];
+  }
+
+const ApprovedProperties:  React.FC<PropertyTableProps> = ({ properties }) => {
+    const handleInfoProperty = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        // Aquí manejas la lógica para el botón "Eliminar"
+      };
+  return (
+    <table className={styles.primary}>
+          <thead>
+            <tr className="bg-gray-200 text-primary  uppercase text-sm leading-normal">
+              <th className="py-3 px-6 text-left">Nombre</th>
+              <th className="py-3 px-6 text-left">Dirección</th>
+              <th className="py-3 px-6 text-center">Ciudad</th>
+              <th className="py-3 px-6 text-center">Precio</th>
+              <th className="py-3 px-6 text-center">Estado</th>
+              <th className="py-3 px-6 text-center">Detalles</th>
+              <th className="py-3 px-6 text-center">Acciones</th>
+            </tr>
+          </thead>
+          <tbody className="text-gray-600 text-sm font-light">
+            {properties.filter((prop: IProperty) => {
+              return prop.propertyStatus === 'approved'
+            }).map((properties: any, i: any) => {
+              return (
+                <tr key={i} className="border-b border-gray-200 hover:bg-gray-100">
+                  <td className="py-3 px-6 text-left text-primary">{properties.propertyName}</td>
+                  <td className="py-3 px-6 text-left">{properties.address}</td>
+                  <td className="py-3 px-6 text-center">{properties.city}</td>
+                  <td className="py-3 px-6 text-center">{properties.price}</td>
+                  <td className="py-3 px-6 text-center">{properties.propertyStatus}</td>
+                  <td className="border px-4 py-2 text-center">
+                    <div className="flex justify-center">
+                      <button className="bg-primary text-secondary px-4 py-2 rounded font-semibold">
+                        Ver más
+                      </button>
+                    </div>
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    <div className="flex justify-center">
+                      <button onClick={handleInfoProperty} className="bg-red-400 text-white px-4 py-2 rounded">
+                        Eliminar
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+  )
+}
+
+export default ApprovedProperties
--- alquilaya/src/app/admin/solicitudes/page.tsx
+++ alquilaya/src/app/admin/solicitudes/page.tsx
@@ -1,117 +1,17 @@
-"use client"
-import HeaderAdmin from '@/Components/HeaderAdmin/HeaderAdmin'
-import styles from "./solicitudes.module.css"
-import Link from 'next/link';
-import IProperty from '@/Interfaces/IProperties';
-import { useEffect, useState } from 'react';
+import HeaderAdmin from '@/Components/HeaderAdmin/HeaderAdmin';
+import PropertiesHistory from '@/Components/PropertiesHistory/PropertiesHistory';
+import PendingPropertiesTable from '@/Components/PendingProperties/PendingProperties';
 
-const url = "http://localhost:3001/property";
+const SolicitudesPage = async () => {
+  const url = "http://localhost:3001/property";
+  
+  const res = await fetch(url, {
+    method: "GET",
+    cache: 'no-store',
+  });
+  if (!res.ok) throw new Error('Error al obtener las propiedades');
 
-const page = () => {
-  const [properties, setProperties] = useState<IProperty[]>([]);
-  const [token, setToken] = useState<string | null>(null);
-
-  useEffect(() => {
-    const storedData = localStorage.getItem("user"); // Aquí se obtiene el string
-
-    if (storedData) {
-      const parsedData = JSON.parse(storedData);
-      const token = parsedData.token;
-      setToken(token);
-    } else {
-      alert("No tienes permiso para esto")
-    }
-
-    const fetchProperties = async () => {
-      const res = await fetch(url, {
-        method: "GET",
-        cache: 'no-store',
-      });
-      if (!res.ok) throw new Error('Error al obtener las propiedades');
-      const data = await res.json();
-      setProperties(data);
-    };
-    fetchProperties();
-  }, []);
-
-  const fetchProperties = async () => {
-    const res = await fetch(url, {
-      method: "GET",
-      cache: 'no-store',
-    });
-    if (!res.ok) throw new Error('Error al obtener las propiedades');
-    const data = await res.json();
-    setProperties(data);
-  };
-
-  const handleApprovedProperty = async (e: any, id: string) => {
-    e.preventDefault();
-
-    const res = await fetch(`${url}/approve/${id}`, {
-      method: "PATCH",
-      cache: 'no-store',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!res.ok) throw new Error('Error al cambiar el estado de la propiedad');
-    const updatedProperty = await res.json();
-
-    alert("Propiedad aprobada")
-
-    fetchProperties();
-
-    setProperties((prevProperties) =>
-      prevProperties.map((property) =>
-        property.id === id ? { ...property, ...updatedProperty } : property
-      )
-    );
-  };
-
-  const handleDisapprovedProperty = async (e: any, id: string) => {
-    e.preventDefault();
-    e.preventDefault();
-
-    const res = await fetch(`${url}/deny/${id}`, {
-      method: "PATCH",
-      cache: 'no-store',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!res.ok) throw new Error('Error al cambiar el estado de la propiedad');
-    const updatedProperty = await res.json();
-
-    alert("Propiedad Denegada exitosamente")
-
-    fetchProperties();
-
-    setProperties((prevProperties) =>
-      prevProperties.map((property) =>
-        property.id === id ? { ...property, ...updatedProperty } : property
-      )
-    );
-  };
-
-  const pendingProperties = properties.filter((prop) => prop.propertyStatus === "pending");
-
-  if (pendingProperties.length === 0) {
-    return (
-      <div>
-        <HeaderAdmin />
-        <div className="container">
-          <div className="padding-section">
-            <h1 className="pb-12 text-primary">Solicitudes</h1>
-            <h4> No tienes solicitudes pendientes</h4>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  const properties = await res.json();
 
   return (
     <div>
@@ -119,88 +19,13 @@ const page = () => {
       <div className="container">
         <div className="padding-section">
           <h1 className="pb-12 text-primary">Solicitudes</h1>
-          <table className={styles.primary}>
-            <thead>
-              <tr className="bg-gray-200 text-primary uppercase text-sm leading-normal">
-                <th className="py-3 px-6 text-left">Nombre Propiedad</th>
-                <th className="py-3 px-6 text-left">Nombre Usuario</th>
-                <th className="py-3 px-6 text-center">Precio</th>
-                <th className="py-3 px-6 text-center">Estado</th>
-                <th className="py-3 px-6 text-center">Detalles</th>
-                <th className="py-3 px-6 text-center">Acciones</th>
-              </tr>
-            </thead>
-            <tbody className="text-gray-600 text-sm font-light">
-              {properties.filter((prop) => prop.propertyStatus === "pending")
-                .map((property, i) => (
-                  <tr key={i} className="border-b border-gray-200 hover:bg-gray-100">
-                    <td className="py-3 px-6 text-left text-primary">{property.propertyName}</td>
-                    <td className="py-3 px-6 text-left">{property.address}</td>
-                    <td className="py-3 px-6 text-center">{property.price}</td>
-                    <td className="py-3 px-6 text-center">{property.propertyStatus}</td>
-                    <td className="border px-4 py-2 text-center">
-                      <div className="flex justify-center">
-                        <button className="bg-primary text-secondary px-4 py-2 rounded font-semibold">
-                          Ver más
-                        </button>
-                      </div>
-                    </td>
-                    <td className="border px-4 py-2 text-center flex justify-center gap-4">
-                      <button
-                        onClick={(e) => handleApprovedProperty(e, property.id)}
-                        className="bg-green-400 text-white px-4 py-2 rounded"
-                      >
-                        Aprobar
-                      </button>
-                      <button onClick={(e) => handleDisapprovedProperty(e, property.id)}
-                        className="bg-red-400 text-white px-4 py-2 rounded">
-                        Denegar
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-          <Link href="/admin">
-            <button className={styles.button}>Volver</button>
-          </Link>
-          <h1 className="py-12 text-primary ">Historial</h1>
-          <table className={styles.primary}>
-          <thead>
-            <tr className="bg-gray-200 text-primary  uppercase text-sm leading-normal">
-              <th className="py-3 px-6 text-left">Nombre</th>
-              <th className="py-3 px-6 text-left">Dirección</th>
-              <th className="py-3 px-6 text-center">Ciudad</th>
-              <th className="py-3 px-6 text-center">Precio</th>
-              <th className="py-3 px-6 text-center">Estado</th>
-              <th className="py-3 px-6 text-center">Detalles</th>
-            </tr>
-          </thead>
-          <tbody className="text-gray-600 text-sm font-light">
-            {properties.map((properties: IProperty, i: any) => {
-              return (
-                <tr key={i} className="border-b border-gray-200 hover:bg-gray-100">
-                  <td className="py-3 px-6 text-left text-primary">{properties.propertyName}</td>
-                  <td className="py-3 px-6 text-left">{properties.address}</td>
-                  <td className="py-3 px-6 text-center">{properties.city}</td>
-                  <td className="py-3 px-6 text-center">{properties.price}</td>
-                  <td className="py-3 px-6 text-center">{properties.propertyStatus}</td>
-                  <td className="border px-4 py-2 text-center">
-                    <div className="flex justify-center">
-                      <button className="bg-primary text-secondary px-4 py-2 rounded font-semibold">
-                        Ver más
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+          <PendingPropertiesTable properties={properties} />
+          <h1 className="py-12 text-primary">Historial</h1>
+          <PropertiesHistory properties={properties} />
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default page
+export default SolicitudesPage;
