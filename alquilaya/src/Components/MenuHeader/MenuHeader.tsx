2b2b03daa1fa01c331139661a75505f208830f88
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-11-03 20:17:07 -0500

    Ensayo cambio texto

--- alquilaya/src/Components/MenuHeader/MenuHeader.tsx
+++ alquilaya/src/Components/MenuHeader/MenuHeader.tsx
@@ -10,7 +10,7 @@ const MenuHeader = () => {
   return (
     <div className={styles.container}>
           <ul className={styles.menu}>
-            <Link href="/nosotros"><li className={styles.itemsnav}> Sobre nosotros</li></Link>
+            <Link href="/nosotros"><li className={styles.itemsnav}> Nosotros</li></Link>
             <Link href="/propiedades"><li className={styles.itemsnav}>Propiedades</li></Link>
             <Link href="/faq"><li className={styles.itemsnav}>FAQ</li></Link>
             <Link href="/sube-tu-propiedad/paso-1"><li className={styles.itemsnav}>Sube tu propiedad</li></Link>
