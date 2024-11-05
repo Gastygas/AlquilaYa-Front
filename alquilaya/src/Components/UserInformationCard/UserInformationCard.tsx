31b7d0e56886bd94612dd9284577c97c24f23c8b
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-11-05 16:55:01 -0500

    dashboard mi cuenta estilos

--- alquilaya/src/Components/UserInformationCard/UserInformationCard.tsx
+++ alquilaya/src/Components/UserInformationCard/UserInformationCard.tsx
@@ -21,34 +21,34 @@ const UserInformationCard =  () => {
     fetchUserData();
   }, []);
 
-  if (error) return (    <div className='w-2/3 flex flex-col bg-cyan-100  p-4 rounded-2xl justify-start align-top'>
-    <h3 className='text-primary text-center opacity-1 pb-4'>Tu Información</h3>
-    <div className='flex flex-row'>
-      <h4 className="text-center">Ha ocurrido un error</h4>
+  if (error) return (    <div className={styles.bigBox}>
+    <h3 className={styles.title}>Tu Información</h3>
+    <div className={styles.grid}>
+      <h4 className={styles.center}>Ha ocurrido un error</h4>
     </div>
   </div>
    
   );
  
   return (
-    <div className='w-2/3 flex flex-col bg-cyan-100  p-4 rounded-2xl justify-center align-middle'>
-    <h3 className='text-primary text-center opacity-1 pb-4'>Tu Información</h3>
-    <div className='flex flex-row'>
-      <div className='w-1/2 flex flex-col opacity-80'>
-        <h4 className='pt-4 text-center font-bold'>Nombre</h4>
-        <h4 className='pb-4 text-center'>{userData?.name} {userData?.surname}</h4>
-        <h4 className='pt-4 text-center font-bold'>Email</h4>
-        <h4 className='pb-4 text-center'>{userData?.email}</h4>
-        <h4 className='pt-4 text-center font-bold'>Dni</h4>
-        <h4 className='pb-4 text-center'>{userData?.dni}</h4>
+    <div className={styles.bigBoxContent}>
+    <h3 className={styles.title}>Tu Información</h3>
+    <div className={styles.grid}>
+      <div className={styles.singleItem}>
+        <h4 className={styles.subtitle}>Nombre</h4>
+        <h4 className={styles.content}>{userData?.name} {userData?.surname}</h4>
+        <h4 className={styles.subtitle}>Email</h4>
+        <h4 className={styles.content}>{userData?.email}</h4>
+        <h4 className={styles.subtitle}>Dni</h4>
+        <h4 className={styles.content}>{userData?.dni}</h4>
       </div>
-      <div className='w-1/2 flex flex-col opacity-80'>
-        <h4 className='pt-4 text-center font-bold'>País</h4>
-        <h4 className='pb-4 text-center'>{userData?.country}</h4>
-        <h4 className='pt-4 text-center font-bold'>Dirección</h4>
-        <h4 className='pb-4 text-center'>{userData?.address}</h4>
-        <h4 className='pt-4 text-center font-bold'>Teléfono</h4>
-        <h4 className='pb-4 text-center'>{userData?.phone}</h4>
+      <div className={styles.singleItem}>
+        <h4 className={styles.subtitle}>País</h4>
+        <h4 className={styles.content}>{userData?.country}</h4>
+        <h4 className={styles.subtitle}>Dirección</h4>
+        <h4 className={styles.content}>{userData?.address}</h4>
+        <h4 className={styles.subtitle}>Teléfono</h4>
+        <h4 className={styles.content}>{userData?.phone}</h4>
       </div>
 
     </div>
