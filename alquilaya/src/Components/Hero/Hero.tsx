e698580509704fde5324bfd6417476e43aa265ba
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-11-05 20:57:48 -0500

    ensayo de centrado 3 hero

--- alquilaya/src/Components/Hero/Hero.tsx
+++ alquilaya/src/Components/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import styles from "./Hero.module.css"
 const Hero = () => {
   return (
     <div className={styles.heroBg} style={{ backgroundImage: `url('/HeroBg.jpg')` }} >
-      <div className="container absolute inset-0 flex justify-center">
+      <div className={styles.containerGrid}>
           <div className={styles.textOrder}>
             <h1 style={{ textShadow: '8px 8px 12px rgba(0, 0, 0, 0.7)' }} className={styles.text} >Explora, Reserva y Disfruta<br/> ¡Así de fácil!</h1>
           </div>
