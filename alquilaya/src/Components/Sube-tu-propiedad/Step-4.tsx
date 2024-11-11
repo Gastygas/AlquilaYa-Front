37b783abd4c9fdee1a3f2264d9395cf048024a4f
Gastygas <gastonrodrigogonzalez55@gmail.com>
2024-11-11 16:00:39 -0300

    google maps en sube tu propiedad

--- alquilaya/src/Components/Sube-tu-propiedad/Step-4.tsx
+++ alquilaya/src/Components/Sube-tu-propiedad/Step-4.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React, { useCallback, useEffect, useState } from 'react';
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import { Loader } from '@googlemaps/js-api-loader'
 import ButtonCyan from '../ButtonCyan/ButtonCyan';
 import { useRouter } from 'next/navigation';
 import ButtonCyanBack from '../ButtonCyan/ButtonCyanBack';
 
-interface PropertyData {
+interface IPropertyData {
   propertyName: string;
   address: string;
   city: string;
@@ -14,8 +15,8 @@ interface PropertyData {
   province: string;
   floor?:string;
   room?:string;
-  mapLocation?: { lat: number; lng: number };
-  invoiceFile?: File | null;
+  mapLocation: { lat: number; lng: number };
+  addressMaps:string;
 }
 
 const containerStyle = {
@@ -23,20 +24,30 @@ const containerStyle = {
   height: '100%',
 };
 
-
-
 const Step4: React.FC = () => {
   const [token, setToken] = useState<string | null>(null);
-
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [marker, setMarker] = useState<google.maps.Marker | null>(null);
+  const router = useRouter();
 
     useEffect(() => {
+      const loader = new Loader({
+        apiKey: `${process.env.NEXT_PUBLIC_MAPS_KEY}`,
+        version: 'weekly',
+        libraries: ['places'],
+      });
+  
+      loader.load().then(() => {
+        initMap();
+      });
         const storedData = localStorage.getItem("user");
         if (storedData) {
           const parsedData = JSON.parse(storedData);
           setToken(parsedData.token);
         } 
       }, []);
-  const [propertyData, setPropertyData] = useState<PropertyData>({
+  
+  const [propertyData, setPropertyData] = useState<IPropertyData>({
     propertyName: '',
     address: '',
     city: '',
@@ -44,92 +55,77 @@ const Step4: React.FC = () => {
     description: '',
     province: '',
     room:'',
-    floor:''
-    // mapLocation: { lat: -34.6037, lng: -58.3816 },
-    // invoiceFile: null,
+    floor:'',
+    mapLocation: { lat: -34.6037, lng: -58.3816 },
+    addressMaps: ''
   });
 
-  const router = useRouter();
+  const initMap = () => {
+    const mapOptions = {
+      center: { lat: -38.005965, lng: -57.544678 },
+      zoom: 15,
+    };
+    const mapDiv = document.getElementById('map') as HTMLElement;
+    const newMap = new google.maps.Map(mapDiv, mapOptions);
+    setMap(newMap);
 
-  // const { isLoaded } = useJsApiLoader({
-  //   id: 'google-map-script',
-  //   googleMapsApiKey: 'AIzaSyDLjvqA9G0qab3gX3I6Fv-aUZ_R26WS-nA',
-  // });
+    const input = document.getElementById('addressMaps') as HTMLInputElement;
+    const autocomplete = new google.maps.places.Autocomplete(input, {
+      fields: ['address_components', 'geometry'],
+      types: ['address'],
+    })
 
-  // const [map, setMap] = useState<google.maps.Map | null>(null);
-
-  // const onLoad = useCallback((map: google.maps.Map) => {
-  //   const bounds = new window.google.maps.LatLngBounds(propertyData.mapLocation);
-  //   map.fitBounds(bounds);
-  //   setMap(map);
-  // }, [propertyData.mapLocation]);
-
-  // const onUnmount = useCallback(() => {
-  //   setMap(null);
-  // }, []);
-
-  // const handleMapClick = (event: google.maps.MapMouseEvent) => {
-  //   const latLng = event.latLng;
-
-  //   if (latLng) {
-  //     setPropertyData((prevData) => ({
-  //       ...prevData,
-  //       mapLocation: {
-  //         lat: latLng.lat(),
-  //         lng: latLng.lng(),
-  //       },
-  //     }));
-  //   }
-  // };
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      if (place.geometry && place.geometry.location) {
+        const location = place.geometry.location;
+        const newCoordinates = { lat: location.lat(), lng: location.lng() };
+        newMap.setCenter(location);
+        if(marker){
+          marker.setPosition(newCoordinates)
+        } else {
+          const newMarker = new google.maps.Marker({
+            position: newCoordinates,
+            map: newMap,
+          });
+          setMarker(newMarker);
+        }
+        const streetNumber = getAddressComponent(place, 'street_number') || '';
+        const route = getAddressComponent(place, 'route') || '';
+        const fullAddress = `${route} ${streetNumber} `.trim();
+        setPropertyData(prevData => ({
+          ...prevData,
+          address: fullAddress,
+          city: getAddressComponent(place, 'locality') || '',
+          country: getAddressComponent(place, 'country') || '',
+          province: getAddressComponent(place, 'administrative_area_level_1') || '',
+          mapLocation: newCoordinates,
+          addressUrl: fullAddress,
+        }));
+      }
+    });
+  };
+  const getAddressComponent = (place: google.maps.places.PlaceResult, type: string) => {
+    const component = place.address_components?.find((comp) => comp.types.includes(type));
+    return component ? component.long_name : '';
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
+  
     setPropertyData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
   };
 
-  // const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   const files = e.target.files;
-
-  //   if (files && files.length > 0) {
-  //     setPropertyData((prevData) => ({
-  //       ...prevData,
-  //       invoiceFile: files[0],
-  //     }));
-  //   }
-  // };
-
-  // const searchAddress = async () => {
-  //   const address = `${propertyData.address}, ${propertyData.city}, ${propertyData.country}`;
-  //   const response = await fetch(
-  //     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=AIzaSyDLjvqA9G0qab3gX3I6Fv-aUZ_R26WS-nA`
-  //   );
-  //   const data = await response.json();
-
-  //   if (data.results.length > 0) {
-  //     const location = data.results[0].geometry.location;
-  //     setPropertyData((prevData) => ({
-  //       ...prevData,
-  //       mapLocation: {
-  //         lat: location.lat,
-  //         lng: location.lng,
-  //       },
-  //     }));
-  //     if (map) {
-  //       map.panTo(location);
-  //     }
-  //   } else {
-  //     alert("Dirección no encontrada. Por favor, verifica los datos.");
-  //   }
-  // };
 
   const handleSubmit = async () => {
     try {
   
       let data = sessionStorage.getItem('data') ? JSON.parse(sessionStorage.getItem('data')!) : {}
-
+      console.log('lng',propertyData.mapLocation.lng,"lat",propertyData.mapLocation.lat);
+      
       const formData = {
         type: data.tipe.toLowerCase(),
         propertyName: propertyData.propertyName.toLowerCase(),
@@ -140,6 +136,8 @@ const Step4: React.FC = () => {
         province: propertyData.province.toLowerCase(),
         floor: propertyData.floor ? propertyData.floor.toLowerCase() : "",
         room: propertyData.room? propertyData.room.toLowerCase() : "",
+        lat: String(propertyData.mapLocation.lat),
+        lng: String(propertyData.mapLocation.lng),
         price: data.price,
         capacity: data.limitCapacity,
         bedrooms: data.bedrooms,
@@ -180,7 +178,7 @@ const Step4: React.FC = () => {
         return
       }
       if(res.message === "Address already used"){
-        alert("Ya existe una propiedad con la misma dirección")
+        alert("Ya existe una propiedad con la misma dirección y/o misma habitacion ")
         return
       }
       else {
@@ -205,8 +203,21 @@ const Step4: React.FC = () => {
         </div>
         <div className="flex justify-center w-full">
   <form className="space-y-6 w-[400px]">
+    <div className="flex flex-col">
+      <label htmlFor="addresMaps">Busca tu direccion aquí</label>
+        <input
+         type="text"
+         id="addressMaps" 
+         name="addressMaps" 
+         onChange={ handleChange} 
+         value={propertyData.addressMaps}
+          placeholder="Dirección"
+          className="border border-[#aa31cf] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#aa31cf] hover:border-[#4DBDFF]" />
 
-    {/* Nombre del Propietario */}
+      </div>
+      <div id="map" style={{ height: '350px', width: '400px' }}></div>
+
+    {/* Nombre de la propiedad */}
     <div className="flex flex-col">
       <label htmlFor="propertyName" className="mb-1 font-medium">Titulo de la propiedad</label>
       <input
@@ -231,7 +242,8 @@ const Step4: React.FC = () => {
         placeholder="Dirección"
         value={propertyData.address}
         onChange={handleChange}
-        className="border border-[#aa31cf] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#aa31cf] hover:border-[#4DBDFF]"
+        className="border border-[#aa31cf] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#aa31cf]"
+        disabled={true}
         required
       />
     </div>
@@ -273,7 +285,8 @@ const Step4: React.FC = () => {
         placeholder="Ciudad"
         value={propertyData.city}
         onChange={handleChange}
-        className="border border-[#aa31cf] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#aa31cf] hover:border-[#4DBDFF]"
+        disabled={true}
+        className="border border-[#aa31cf] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#aa31cf]"
         required
       />
     </div>
@@ -288,7 +301,8 @@ const Step4: React.FC = () => {
         placeholder="Provincia"
         value={propertyData.province}
         onChange={handleChange}
-        className="border border-[#aa31cf] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#aa31cf] hover:border-[#4DBDFF]"
+        disabled={true}
+        className="border border-[#aa31cf] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#aa31cf]"
         required
       />
     </div>
@@ -303,7 +317,8 @@ const Step4: React.FC = () => {
         placeholder="País"
         value={propertyData.country}
         onChange={handleChange}
-        className="border border-[#aa31cf] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#aa31cf] hover:border-[#4DBDFF]"
+        disabled={true}
+        className="border border-[#aa31cf] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#aa31cf]"
         required
       />
     </div>
@@ -321,54 +336,13 @@ const Step4: React.FC = () => {
         required
       />
     </div>
-
-    {/* Subir Factura para Justificación de Dirección */}
-    {/* <div className="flex flex-col">
-      <label htmlFor="invoiceFile" className="mb-1 font-medium">Subir factura que coincida con la dirección</label>
-      <input
-        type="file"
-        id="invoiceFile"
-        name="invoiceFile"
-        onChange={handleFileChange}
-        className="bg-white border border-[#aa31cf] p-2 rounded-lg focus:outline-none focus:ring-1 focus:ring-[#aa31cf] cursor-pointer hover:border-[#4DBDFF]"
-        required
-      />
-    </div> */}
-
   </form>
 </div>
-
-
-        {/* Botón de búsqueda de dirección */}
-        {/* <button
-          onClick={searchAddress}
-          className="mt-4 w-full py-2 text-white bg-blue-500 hover:bg-blue-600 rounded-lg font-semibold"
-        >
-          Buscar dirección en el mapa
-        </button> */}
-
-        {/* Mapa de Google */}
-        {/* <div className="w-full h-64 my-6 rounded-lg overflow-hidden"> */}
-          {/* {isLoaded ? (
-            <GoogleMap
-              mapContainerStyle={containerStyle}
-              center={propertyData.mapLocation}
-              zoom={10}
-              onLoad={onLoad}
-              onUnmount={onUnmount}
-              onClick={handleMapClick}
-            >
-              <Marker position={propertyData.mapLocation} />
-            </GoogleMap>
-          ) : (
-            <p className="text-center text-gray-500">Cargando mapa...</p>
-          )} */}
-        {/* </div> */}
       </div>
       <div className="absolute bottom-1/2 right-6">
         <ButtonCyan 
         onClick={handleSubmit}
-        isDisabled={false}
+        isDisabled={propertyData.address === "" || propertyData.description === "" || propertyData.propertyName === ""}
          />
       </div>
 
