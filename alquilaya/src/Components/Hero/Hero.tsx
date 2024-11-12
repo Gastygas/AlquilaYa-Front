377115c90e5f5a63abb3ecb1f426f3df8913abab
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-11-11 22:48:15 -0500

    Hero centrado 2

--- alquilaya/src/Components/Hero/Hero.tsx
+++ alquilaya/src/Components/Hero/Hero.tsx
@@ -4,10 +4,8 @@ const Hero = () => {
   return (
     <div className={styles.heroBg} style={{ backgroundImage: `url('/HeroBg.jpg')` }} >
       <div className={styles.containerGrid}>
-        <div className="padding-section">
           <div className={styles.textOrder}>
             <h1 style={{ textShadow: '8px 8px 12px rgba(0, 0, 0, 0.7)' }} className={styles.text} >Explora, Reserva y Disfruta<br/> ¡Así de fácil!</h1>
-          </div>
           </div>
       </div>
     </div >
