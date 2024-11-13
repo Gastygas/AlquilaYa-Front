40d9c81326c69ef2e883c3854668f1532f734844
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-11-13 13:38:28 -0500

    Lógica denegar usuario

--- alquilaya/src/app/admin/usuarios/page.tsx
+++ alquilaya/src/app/admin/usuarios/page.tsx
@@ -1,6 +1,7 @@
 import HeaderAdmin from '@/Components/HeaderAdmin/HeaderAdmin'
 import styles from "./usuario.module.css"
 import Link from 'next/link';
+import UserList from '@/Components/UserList/UserList';
 
 const page = async () => {
 
@@ -11,56 +12,24 @@ const page = async () => {
   if (!res.ok) throw new Error('Error al obtener los usuarios');
   
   const users = await res.json();
+    /*const notifydeclineProperty = () => toast.success("Usuario Eliminado exitosamente", { autoClose: 3000 });
+
+  const handleDenyUser = async (e: React.MouseEvent, id: string) => {
+    e.preventDefault();
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACK_URL}/users/disable/${id}`, {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!res.ok) throw new Error('Error al cambiar el estado de la propiedad');
+    notifydeclineProperty();
+  };*/
 
   return (
     <div>
       <HeaderAdmin />
-      <div className='container'>
-        <div className='padding-section'>
-          <h1 className="pb-12 text-primary">Usuarios</h1>
-          <table className={styles.primary}>
-            <thead>
-              <tr className="bg-gray-200 text-primary  uppercase text-sm leading-normal">
-                <th className="py-3 px-6 text-left">Nombre</th>
-                <th className="py-3 px-6 text-left">Email</th>
-                <th className="py-3 px-6 text-center">Dni</th>
-                <th className="py-3 px-6 text-center">País</th>
-                <th className="py-3 px-6 text-center">Dirección</th>
-                <th className="py-3 px-6 text-center">Teléfono</th>
-                <th className="py-3 px-6 text-center">Favoritos</th>
-
-              </tr>
-            </thead>
-            <tbody className="text-gray-600 text-sm font-light">
-            {users.map((user: any, i: any) => {
-              return (
-                <tr key={i} className="border-b border-gray-200 hover:bg-gray-100">
-                  <td className="py-3 px-6 text-left text-primary">{user.name} {user.surname}</td>
-                  <td className="py-3 px-6 text-left">{user.email}</td>
-                  <td className="py-3 px-6 text-center">{user.dni}</td>
-                  <td className="py-3 px-6 text-center">{user.country}</td>
-                  <td className="py-3 px-6 text-center">{user.address}</td>
-                  <td className="border px-4 py-2 text-center flex justify-center gap-4">
-              <button
-               /* onClick={(e) => handleDisapprovedProperty(e, property.id)}*/
-                className={styles.deny}>Eliminar</button>
-            </td>
-                  <td className="border px-4 py-2 text-center">
-                    <div className="flex justify-center">
-                    <Link href={`/admin/usuarios/${user.id}`}>
-                      <button className="bg-primary text-secondary px-4 py-2 rounded font-semibold">
-                        Ver más
-                      </button></Link>
-                    </div>
-                  </td>
-                </tr>
-              )
-            })}
-            </tbody>
-          </table>
-          <Link href="/admin"><button className={styles.button}>Volver</button></Link>
-        </div>
-      </div>
+      <UserList users={users}/>
     </div >
   )
 }
