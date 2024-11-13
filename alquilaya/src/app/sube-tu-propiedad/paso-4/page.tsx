48378e60054ea98e17fcb7d863188ae4a48ac859
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-11-13 15:53:21 -0500

    Sube tu propiedad protegido

--- alquilaya/src/app/sube-tu-propiedad/paso-4/page.tsx
+++ alquilaya/src/app/sube-tu-propiedad/paso-4/page.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Step4 from '@/Components/Sube-tu-propiedad/Step-4'
 import Header from '@/Components/Header/Header';
+import ProtectedRoute from '@/Components/ProtectRoutes/ProtecRoutes';
 
 const paso4 = () => {
   return (
+    <ProtectedRoute>
     <div className='bg-gray-100'>
     <Header />
       <div className='container'>
         <Step4/>
       </div>
     </div>
+    </ProtectedRoute>
   )
 }
 
--- alquilaya/src/app/sube-tu-propiedad/paso-5/page.tsx
+++ alquilaya/src/app/sube-tu-propiedad/paso-5/page.tsx
@@ -3,9 +3,11 @@ import Step5 from '@/Components/Sube-tu-propiedad/Step-5'
 import React, { Suspense } from 'react'
 import styles from './step5.module.css'
 import Loader from '@/Components/Loader/Loader';
+import ProtectedRoute from '@/Components/ProtectRoutes/ProtecRoutes';
 
 const paso5 = () => {
   return (
+    <ProtectedRoute>
     <div className='bg-gray-100'>
     <Header />
     {/* <div className={styles.container}> */}
@@ -15,6 +17,7 @@ const paso5 = () => {
       </Suspense>
       </div>
     </div>
+    </ProtectedRoute>
   )
 }
 
