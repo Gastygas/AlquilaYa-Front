939c38a6353bf94f446e32e639a57c2a0ebd8ca3
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-11-13 16:03:28 -0500

    Arreglo bug logout

--- alquilaya/src/Components/ProtectRoutes/ProtecRoutes.tsx
+++ alquilaya/src/Components/ProtectRoutes/ProtecRoutes.tsx
@@ -4,27 +4,30 @@ import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import AuthContext from "../contexts/authContext";
 import Loader from "../Loader/Loader";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const ProtectedRoute = ({ children, adminOnly = false }: { children: React.ReactNode, adminOnly?: boolean }) => {
   const router = useRouter();
   const { user, loading } = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState(true);
+  const notifyLogOutTrue = () => toast.success("Has Cerrado Sesión exitosamente", { autoClose: 3000 });
 
   useEffect(() => {
     if (!loading) {
-    if (!user?.user) {
-      //router.push("/login");
-      alert("no autorizo")
-    } else if (adminOnly && !user?.user.isAdmin) {
-      router.push("/");
-    } else {
-      setIsLoading(false);
+      if (!user?.user) {
+        notifyLogOutTrue()
+        router.push("/login");
+      } else if (adminOnly && !user?.user.isAdmin) {
+        router.push("/");
+      } else {
+        setIsLoading(false);
+      }
     }
-  }
   }, [user, loading, router, adminOnly]);
 
   if (isLoading || loading) {
-    return <Loader/>;
+    return <Loader />;
   }
   return <>{children}</>;
 };
