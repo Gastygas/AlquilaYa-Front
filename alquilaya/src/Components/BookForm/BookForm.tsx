d006c4dfc5cfbaa9db0ed342b07472e80df4a200
benjadelcampo15 <benjadelcampo15@gmail.com>
2024-11-15 14:41:59 -0300

    intento

--- alquilaya/src/Components/BookForm/BookForm.tsx
+++ alquilaya/src/Components/BookForm/BookForm.tsx
@@ -64,7 +64,7 @@ const BookForm: React.FC<BookFormProps> = ({ propertyId, propertyName, unitPrice
     );
     console.log(
       "Fechas excluidas actualizadas (DD/MM/YYYY):",
-      excludedDates.map((date) => date.toLocaleDateString("en-GB"))
+      excludedDates.map((date) => date.toLocaleDateString("en-AR"))
     );
   }, [excludedDates]);
 
